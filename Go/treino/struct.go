d563299c22ea3e4581b8f31e66f7b3f841b67e0b
gabriel Henrique <gabriel.hr@sempreceub.com>
2024-03-31 00:04:24 -0300

    dsd

---
+++ Go/treino/struct.go
@@ -0,0 +1,33 @@
+package main
+
+import ("fmt", "error")
+
+
+type Cachorro struct{
+	idade int
+	nome string
+
+
+}
+dfs
+func divisão(n int,n2 int,err error){
+	if (n2 == 0){
+	return 0, errors.New("Não pode dividir por 10")
+}
+	return n/n2, nil
+
+
+}
+
+func main () {
+	var husky Cachorro
+	husky.idade = 12
+	husky.nome = "holy"
+
+	fmt.Println(husky.idade, "---", husky.nome)
+
+
+	divisão(10)
+}
+
+
